a8b196337bf5218434df5d50b1847c8c56c95b6b
Andrew Smith <ajsmith8@gmail.com>
2012-04-16 14:46:49 -0500

    first

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,25 @@
+class TopicsController < ApplicationController
+  def index
+    @title = "All Topics"
+    @topics = Topic.all
+  end
+  
+  def new
+    @title = "New Topic"
+    @topic = Topic.new
+  end
+  
+  def create
+    @topic = Topic.new(params[:topic])
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to topics_path, notice: 'Topic was successfully created.' }
+        format.json { render json: @topic, status: :created, location: @topic }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobile4::Application.config.secret_token = '62beabd2a1f09a5737897ad9103972125c14512021bb27d741550f3d29f30047c5892330c706f1455de9fdf7878b948dc27f25ff6f7ee3655b59121af0d0a3b4'
